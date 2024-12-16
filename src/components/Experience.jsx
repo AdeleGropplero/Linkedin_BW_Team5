bdcc8513932ca509b2a66ef7021ed6a44398d98a
Andrea Saccomanni <Andrea Saccomanni>
2024-12-16 16:41:11 +0100

    experiences

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -13,7 +13,7 @@ const Experience = () => {
             <div className="ExperienceIcon p-1 rounded-circle me-2">
               <i className="bi bi-plus-lg fs-2 p-2"></i>
             </div>
-            <div className=" ExperienceIcon p-1 rounded-circle ">
+            <div className="ExperienceIcon p-1 rounded-circle ">
               <i className="bi bi-pencil-fill fs-4 p-2"></i>
             </div>
           </div>
@@ -55,26 +55,26 @@ const Experience = () => {
             <a href="#">
               <img
                 width="48"
-                src="https://media.licdn.com/dms/image/v2/D4D0BAQHuvou30YRddw/company-logo_100_100/company-logo_100_100/0/1720712553131/ntt_data_europe_latam_logo?e=1742428800&amp;v=beta&amp;t=JrE9R7EtrEZxMjyudIFvLHKbgVenJp9csWWeyyaaIWk"
+                src="https://media.licdn.com/dms/image/v2/C4D0BAQG7dO4tG79mdA/company-logo_100_100/company-logo_100_100/0/1668154554619/iconsulting_logo?e=1742428800&amp;v=beta&amp;t=s0hR2HGdC83no2zQfEYY5HFkw3pOITRhtBonvb_GJZ8"
                 loading="lazy"
                 height="48"
-                alt="Logo di NTT DATA Europe &amp; Latam"
-                id="ember541"
+                alt="Logo di ICONSULTING"
+                id="ember266"
                 className="ivm-view-attr__img--centered EntityPhoto-square-3   evi-image lazy-image ember-view"
               ></img>
             </a>
           </div>
           <div className="d-flex flex-column">
-            <h4 className="fs-5 ms-2 mt-2 mb-0 fw-semibold">Consulente SaleForce</h4>
-            <p className="fs-6 ms-2 mt-0 mb-0 ">NTT DATA Italia · A tempo pieno</p>
-            <p className="fs-6 ms-2 mt-0 text-muted">ott 2022 - feb 2024 · 1 anno 5 mesiott 2022 - feb 2024 · 1 anno 5 mesi Roma · Da remoto</p>
+            <h4 className="fs-5 ms-2 mt-2 mb-0 fw-semibold">Software Engineer</h4>
+            <p className="fs-6 ms-2 mt-0 mb-0 ">ICONSULTING · A tempo pieno</p>
+            <p className="fs-6 ms-2 mt-0 text-muted">gen 2022 - feb 2024 · 2 anni 2 mesi - Roma</p>
             <div className="d-flex ms-2 gap-2 fw-semibold">
               <div>
                 <i className="bi bi-gem"></i>
               </div>
               <div>
                 <a href="#" className="Experience text-decoration-none text-black">
-                  <p>SQL, Database e +4 competenze</p>
+                  <p>Sviluppo di software, Database e +7 competenze</p>
                 </a>
               </div>
             </div>
