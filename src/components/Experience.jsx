5b21221fab9cae3c3a803c20237fbc71043e836c
Andrea Saccomanni <Andrea Saccomanni>
2024-12-16 16:22:07 +0100

    .

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -48,6 +48,38 @@ const Experience = () => {
             </div>
           </div>
         </div>
+        <hr />
+        <div className="d-flex align-items-center justify-content-between"></div>
+        <div className="d-flex">
+          <div className=" bg-primary ms-2 mt-2" style={{ height: "48px", width: "48px" }}>
+            <a href="#">
+              <img
+                width="48"
+                src="https://media.licdn.com/dms/image/v2/D4D0BAQHuvou30YRddw/company-logo_100_100/company-logo_100_100/0/1720712553131/ntt_data_europe_latam_logo?e=1742428800&amp;v=beta&amp;t=JrE9R7EtrEZxMjyudIFvLHKbgVenJp9csWWeyyaaIWk"
+                loading="lazy"
+                height="48"
+                alt="Logo di NTT DATA Europe &amp; Latam"
+                id="ember541"
+                className="ivm-view-attr__img--centered EntityPhoto-square-3   evi-image lazy-image ember-view"
+              ></img>
+            </a>
+          </div>
+          <div className="d-flex flex-column">
+            <h4 className="fs-5 ms-2 mt-2 mb-0 fw-semibold">Consulente SaleForce</h4>
+            <p className="fs-6 ms-2 mt-0 mb-0 ">NTT DATA Italia · A tempo pieno</p>
+            <p className="fs-6 ms-2 mt-0 text-muted">ott 2022 - feb 2024 · 1 anno 5 mesiott 2022 - feb 2024 · 1 anno 5 mesi Roma · Da remoto</p>
+            <div className="d-flex ms-2 gap-2 fw-semibold">
+              <div>
+                <i className="bi bi-gem"></i>
+              </div>
+              <div>
+                <a href="#" className="Experience text-decoration-none text-black">
+                  <p>SQL, Database e +4 competenze</p>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
