4a2701eb117d0c30a488fd81de15a1d66f761eaf
Andrea Saccomanni <Andrea Saccomanni>
2024-12-18 17:44:04 +0100

    .

--- src/redux/actions/fetchProfile.js
+++ src/redux/actions/fetchProfile.js
@@ -1,11 +1,10 @@
 const token = import.meta.env.VITE_AUTH_TOKEN;
+
 export const fetchProfile = (userId) => async (dispatch) => {
   dispatch({ type: "FETCH_PROFILE_REQUEST" });
 
   try {
-    const url = userId
-      ? `https://striveschool-api.herokuapp.com/api/profile/${userId}`
-      : "https://striveschool-api.herokuapp.com/api/profile/me";
+    const url = userId ? `https://striveschool-api.herokuapp.com/api/profile/${userId}` : "https://striveschool-api.herokuapp.com/api/profile/me";
 
     const response = await fetch(url, {
       method: "GET",
