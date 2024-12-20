89f003118a4c5b9efdf7b65804f923b7e17921de
Andreea379 <andreeasamoila379@gmail.com>
2024-12-20 10:41:54 +0100

    job page style done

--- src/components/jobs.jsx
+++ src/components/jobs.jsx
@@ -3,11 +3,16 @@ import { useSelector } from "react-redux";
 /* import { fetchJobs } from "../redux/actions/jobsActions"; */
 import { Button, Card, Col, Container, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
+
+import linkedin from "../assets/images/linkedin.png";
 import {
+  BsArrowRight,
   BsBookmarkFill,
+  BsCaretDownFill,
   BsFileEarmarkTextFill,
   BsListUl,
-  BsPencilSquare
+  BsPencilSquare,
+  BsShieldCheck
 } from "react-icons/bs";
 import prime from "../assets/prime.svg";
 import { BiX } from "react-icons/bi";
@@ -135,19 +140,62 @@ const Jobs = () => {
           <div className="d-flex justify-content-between bg-white border border-2 rounded-3 mb-3">
             <div className="ms-2">
               <h5 className="mb-0 mt-3">Recent jobs search</h5>
-              <p className="text-secondary pb-2" style={{ fontSize: "14px" }}>
+              <p className="text-secondary m-0" style={{ fontSize: "14px" }}>
                 Filmmaker
               </p>
-              <Button className="rounded-pill fw-medium px-3 py-1 mb-2">
-                Create alert
+              <p
+                className="text-secondary pb-2 lead"
+                style={{ fontSize: "14px" }}
+              >
+                Italy
+              </p>
+            </div>
+            <BiX className="mt-3 me-2 fs-4" />
+          </div>
+          <div className="d-flex justify-content-between bg-white border border-2 rounded-3 mb-3">
+            <img
+              id="post_image-home-center"
+              className=" w-75 align-bottom z-10 shadow object-fit-contain rounded-circle"
+              src={profileData?.image || "https://via.placeholder.com/35"}
+              alt="profile image"
+            />
+            <div>
+              <p>
+                See the full list of jobs where you would be a top applicant
+              </p>
+              <Button
+                className="rounded-pill border border-0 text-dark px-3 py-1"
+                style={{ backgroundColor: "#f7c477" }}
+              >
+                Try Premium for €0
+              </Button>
+            </div>
+          </div>
+          <div className="d-flex justify-content-between bg-white border border-2 rounded-3 mb-3">
+            <div className="ms-2">
+              <h5 className="mb-0 mt-3">
+                Verify to stand out in your job <br /> search
+              </h5>
+              <p className="text-secondary pb-2" style={{ fontSize: "14px" }}>
+                Verified members get 60% more profile views on
+                <br /> average.
+              </p>
+              <Button className="rounded-pill fw-medium px-3 py-1 mb-2 d-flex align-items-center">
+                <BsShieldCheck className="me-2" />
+                Verify now
               </Button>
             </div>
             <BiX className="mt-3 me-2 fs-4" />
           </div>
-          <h1>Jobs</h1>
 
-          {isLoading && <p>Loading...</p>}
-          {error && <p style={{ color: "red" }}>{error}</p>}
+          <h1 className="d-none">Jobs</h1>
+
+          {isLoading && <p className="d-none">Loading...</p>}
+          {error && (
+            <p className="d-none" style={{ color: "red" }}>
+              {error}
+            </p>
+          )}
 
           <div>
             {jobs.length > 0
@@ -181,7 +229,100 @@ const Jobs = () => {
               : !isLoading && <p>Job not found</p>}
           </div>
         </Col>
-        <Col lg={3}></Col>
+        <Col lg={3} className="bg-white border border-2 rounded-3 mb-3">
+          <p className="fw-medium mt-3 mb-0">Job seeker guidance</p>
+          <p className="text-secondary pb-2" style={{ fontSize: "14px" }}>
+            Recommended based on your activity
+          </p>
+          <div>
+            <p className="fw-semibold">I want to impruve my resume</p>
+          </div>
+          <p>
+            Explore our curated guide of expert-led courses, such as how to
+            improveyour resume and grow your network to help you land your next
+            opportunity.
+          </p>
+          <span className="text-secondary fw-medium">
+            Show more <BsArrowRight className="text-dark" />
+          </span>
+          <div
+            className="d-flex flex-wrap justify-content-center text-secondary mt-3 position-sticky "
+            style={{ fontSize: "90%", top: "71px", zIndex: "1050" }}
+          >
+            <div>
+              <a
+                href="#"
+                className="me-3 mb-1 text-decoration-none text-secondary"
+              >
+                About
+              </a>
+              <a
+                href="#"
+                className="me-3 mb-1 text-decoration-none text-secondary"
+              >
+                Accessibility
+              </a>
+              <a
+                href="#"
+                className="me-2 mb-1 text-decoration-none text-secondary"
+              >
+                Help Center
+              </a>
+
+              <a
+                href="#"
+                className="me-3 mb-1 text-decoration-none text-secondary"
+              >
+                Privacy &amp; Terms{" "}
+                <BsCaretDownFill style={{ fontSize: "90%" }} />
+              </a>
+              <a
+                href="#"
+                className="me-4 mb-1 text-decoration-none text-secondary"
+              >
+                Ad Choices
+              </a>
+
+              <a
+                href="#"
+                className="me-2 mb-1 text-decoration-none text-secondary"
+              >
+                Advertising
+              </a>
+              <a
+                href="#"
+                className="me-2 mb-1 text-decoration-none text-secondary"
+              >
+                Business Services{" "}
+                <BsCaretDownFill style={{ fontSize: "90%" }} />
+              </a>
+
+              <a
+                href="#"
+                className="me-3 mb-1 text-decoration-none text-secondary"
+              >
+                Get the LinkedIn app
+              </a>
+              <a
+                href="#"
+                className="me-2 mb-1 text-decoration-none text-secondary"
+              >
+                More
+              </a>
+              <a
+                href="#"
+                className=" d-flex mt-2 text-dark text-decoration-none justify-content-center align-items-center"
+              >
+                <img
+                  src={linkedin}
+                  alt="linkedin logo"
+                  style={{ width: "20%", height: "80%" }}
+                />
+                LinkedIn Corporation &copy; 2024
+              </a>
+            </div>
+          </div>
+        </Col>
       </Row>
     </Container>
   );
